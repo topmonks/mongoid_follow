404411a09925fba0c5d29a473a9871fc57273535
Jan Uhlar <jan.uhlar@topmonks.com>
2015-08-11 21:54:17 +0200

    [REM] index

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -16,6 +16,4 @@ class Follow
   scope :by_followee, ->(klass) { where(followee_type: klass.to_s) }
 
   index({relation: 1})
-  index({follower_id: 1, follower_type: 1, relation: 1}, { unique: true, drop_dups: true })
-  index({followee_id: 1, follower_type: 1, follower_id: 1, relation: 1}, { unique: true, drop_dups: true })
 end
